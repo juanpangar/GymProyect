ca9fc9a243a0bc2747fa2b48a9852fdf3d47c6a7
Juan Antonio <juanpangar@gmail.com>
2016-05-02 19:05:58 +0200

    Ventana de loging realizada, entramos en la ventana de inicio. La siguiente tarea sera crear la base de datos.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GymProject
+{
+    partial class LogApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogApp));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.accesButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(230, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(240, 294);
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.Size = new System.Drawing.Size(180, 20);
+            this.passBox.TabIndex = 1;
+            this.passBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Introducca la contraseña:";
+            // 
+            // accesButton
+            // 
+            this.accesButton.Location = new System.Drawing.Point(287, 329);
+            this.accesButton.Name = "accesButton";
+            this.accesButton.Size = new System.Drawing.Size(86, 23);
+            this.accesButton.TabIndex = 3;
+            this.accesButton.Text = "Acceder";
+            this.accesButton.UseVisualStyleBackColor = true;
+            this.accesButton.Click += new System.EventHandler(this.accesButton_Click);
+            // 
+            // LogApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 394);
+            this.Controls.Add(this.accesButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LogApp";
+            this.Text = "Gym SEUL";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button accesButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymProject
+{
+    public partial class LogApp : Form
+    {
+        public LogApp()
+        {
+            InitializeComponent();
+        }
+
+        private void accesButton_Click(object sender, EventArgs e)
+        {
+            if(passBox.Text.Trim() == "pass")
+            {
+                this.Hide();
+                Inicio inicio = new Inicio();
+                inicio.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Contraseña errona, por favor vuelva a intentarlo.", "Error Contraseña", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                passBox.Clear();
+            }
+
+        }
+
+        private void pressEnter(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                if (passBox.Text.Trim() == "pass")
+                {
+                    this.Hide();
+                    Inicio inicio = new Inicio();
+                    inicio.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Contraseña errona, por favor vuelva a intentarlo.", "Error Contraseña", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    passBox.Clear();
+                }
+            }
+        }
+    }
+}
